3493fb1c92f664f88314fa4a5a02a16919407c1c
Robin Clark <robindtclark@gmail.com>
2020-09-07 19:09:34 +0100

    add organisation_name to jobs endpoints

--- rails_backend/app/controllers/jobs_controller.rb
+++ rails_backend/app/controllers/jobs_controller.rb
@@ -3,18 +3,26 @@ class JobsController < ApplicationController
 
   # /jobs
   def index
-    org = Job.all
-    render json: org
+    @jobs = Job.all
+    @jobs_data = []
+    @jobs.each do |job|
+      org_name = {"organisation_name" => job.organisation.organisation_name}
+      job = JSON::parse(job.to_json).merge(org_name)
+      @jobs_data << job
+    end
+    render json: @jobs_data
   end
 
   # jobs/:id
   def show
-    render json: @job
+    # render json: @job
+  render json: @job.attributes.merge({
+    organisation_name: @job.organisation.organisation_name })
   end
 
   private
 
-  def set_org
+  def set_job
     @job = Job.find(params[:id])
   end
 end
