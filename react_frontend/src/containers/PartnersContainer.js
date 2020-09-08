3fdf938ae89bbb4f3565e0b00ff02ea3043ed8ea
Neil Mackenzie <nmackenzie@live.co.uk>
2020-09-08 13:40:19 +0100

    removed unlinked icons from table

--- react_frontend/src/containers/PartnersContainer.js
+++ react_frontend/src/containers/PartnersContainer.js
@@ -29,14 +29,18 @@ class PartnersContainer extends React.Component {
     renderTableData() {
         return this.state.org_info.map((org, index) => {
             const {id, organisation_name, homepage, about, job_page_url, linkedin, open_to_spec_app} = org
+            const linkedin_icon = linkedin ? <FontAwesomeIcon icon={ faLinkedin }></FontAwesomeIcon> : null
+            const open_to_spec = open_to_spec_app ? <FontAwesomeIcon icon={ faCheck }></FontAwesomeIcon> : null
+            const job_url_icon = job_page_url ? <FontAwesomeIcon icon={ faBriefcase, faPooStorm }></FontAwesomeIcon> : null
+            const abouticon = about ? <FontAwesomeIcon icon={ faInfo }></FontAwesomeIcon> : null
 
             return (
                 <tr key={id} className="table">
                     <th className="col-width"><a href={homepage}>{organisation_name}</a></th>
-                    <td id="tbody-center"><a href={about}><FontAwesomeIcon icon={ faInfo }></FontAwesomeIcon></a></td>
-                    <td id="tbody-center"><a href={job_page_url}><FontAwesomeIcon icon={ faBriefcase, faPooStorm }></FontAwesomeIcon></a></td>
-                    <td id="tbody-center"><a href={linkedin}><FontAwesomeIcon icon={ faLinkedin }></FontAwesomeIcon></a></td>
-                    <td id="tbody-center">{open_to_spec_app ? <FontAwesomeIcon icon={ faCheck }></FontAwesomeIcon> : <FontAwesomeIcon icon={ faTimesCircle }></FontAwesomeIcon>}</td>
+                    <td id="tbody-center"><a href={about}>{abouticon}</a></td>
+                    <td id="tbody-center"><a href={job_page_url}>{job_url_icon}</a></td>
+                    <td id="tbody-center"><a href={linkedin}>{linkedin_icon}</a></td>
+                    <td id="tbody-center">{open_to_spec}</td>
                 </tr>
             )
         })
