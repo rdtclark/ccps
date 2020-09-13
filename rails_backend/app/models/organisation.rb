1e85ce87e10ee6116d9adfd35c970d81d615ef55
Robin Clark <robindtclark@gmail.com>
2020-09-13 16:21:14 +0100

    LI multi scraper working

--- rails_backend/app/models/organisation.rb
+++ rails_backend/app/models/organisation.rb
@@ -29,4 +29,18 @@ class Organisation < ApplicationRecord
             org.save!
         end
     end
+
+    def self.linkedin_job_urls
+        orgs = Organisation.all
+
+        job_urls = []
+
+        orgs.each do |org|
+            param = org.organisation_name.parameterize(separator: '%20', preserve_case: true)
+            url = "https://uk.linkedin.com/jobs/search?keywords=#{param}&location=Edinburgh%2C%20Scotland%2C%20United%20Kingdom&geoId=100209086&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
+            job_urls << url
+        end
+
+        return job_urls
+    end
 end
--- rails_backend/scrapers/linkedin_spider.rb
+++ rails_backend/scrapers/linkedin_spider.rb
@@ -4,24 +4,18 @@ require 'pry'
 class LinkedinSpider < Kimurai::Base
     @name = "linkedin_spider"
     @engine = :selenium_chrome
-    @start_urls = ["https://uk.linkedin.com/jobs/search?keywords=Junior%20Software%20Engineer&location=Edinburgh%2C%20Scotland%2C%20United%20Kingdom&geoId=&trk=public_jobs_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"]
+    @start_urls = Organisation.linkedin_job_urls[0..20]
     @config = {
-        disable_images: true
+        disable_images: true,
+        delay: 4..7
     }
 
     def parse(response, url:, data: {})
-
-        # find the correct Organisation
-        # org_id = Organisation.where(organisation_name: 'AutoRek').first.id
-        # org = Organisation.find(org_id)
-
-        #jobs_count = response.css('div.careers-table__row').count
 
         returned_jobs = response.css('ul.jobs-search__results-list')
         
         returned_jobs.css('li').each do |element|
 
-            binding.pry
 
             title = element.css('h3').text.strip
 
@@ -29,25 +23,13 @@ class LinkedinSpider < Kimurai::Base
 
             details_url = element.css('a').first["href"]
 
-            #full_details_url = "https://autorek.com" + details_url
-
-            Job.where(organisation_id: org_id, title: title).first_or_create(
-                organisation_id: org_id,
+            Job.where(title: title).first_or_create(
+                organisation_id: 58,
                 title: title,
                 location: location,
-                details_url: full_details_url
+                details_url: details_url
             )
         end
 
-        # update vacancies_listed at Organisation if no jobs listed
-        # if jobs_count > 0
-        #     org.vacancies_listed = true
-        #     org.save!
-        # else
-        #     org.vacancies_listed = false
-        #     org.save!
-        #     org.vacancies_listed = false
-        #     org.save!
-        # end
     end
 end
