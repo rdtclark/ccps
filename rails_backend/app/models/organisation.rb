f44b340489d42979a77742d36315fd8fb166b92e
Robin Clark <robindtclark@gmail.com>
2020-09-21 08:29:02 +0100

    add industry flags to linkedin urls

--- rails_backend/app/models/organisation.rb
+++ rails_backend/app/models/organisation.rb
@@ -14,7 +14,7 @@ class Organisation < ApplicationRecord
         orgs.each do |org|
             param = org.organisation_name.parameterize(separator: '%20', preserve_case: true)
             org_id = org.id
-            url = "https://uk.linkedin.com/jobs/search?keywords=%22#{param}%22&location=Edinburgh%2C%20Scotland%2C%20United%20Kingdom&geoId=100209086&trk=public_jobs_jobs-search-bar_search-submit&id=#{org_id}&redirect=false&position=1&pageNum=0"
+            url = "https://uk.linkedin.com/jobs/search?f_I=4%2C43%2C96&keywords=%22#{param}%22&location=Edinburgh%2C%20Scotland%2C%20United%20Kingdom&geoId=100209086&trk=public_jobs_jobs-search-bar_search-submit&id=#{org_id}&redirect=false&position=1&pageNum=0"
             job_urls << url
         end
 
