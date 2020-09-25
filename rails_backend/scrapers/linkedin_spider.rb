bae3bfb81c77d7a8e3d14f28d2da5abe6eeea04a
Neil Mackenzie <nmackenzie@live.co.uk>
2020-09-25 12:47:22 +0100

    updated LI URL

--- rails_backend/scrapers/linkedin_spider.rb
+++ rails_backend/scrapers/linkedin_spider.rb
@@ -11,7 +11,7 @@ end
 class LinkedinSpider < Kimurai::Base
     @name = "linkedin_spider"
     @engine = :selenium_chrome
-    @start_urls = Organisation.linkedin_job_urls.sample(15)
+    @start_urls = Organisation.linkedin_job_urls.sample(150)
     @config = {
         user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36",
         disable_images: true,
