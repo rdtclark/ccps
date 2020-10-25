c574d8a112ad463ab442e85a5b39d0827d1eedeb
Neil Mackenzie <nmackenzie@live.co.uk>
2020-10-25 13:05:46 +0000

    updated home page

--- react_frontend/src/components/Home.js
+++ react_frontend/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import './Home.css';
 
 const Home = () => (
-    <div>
-        <h2>Welcome</h2>
+    <div className="title">
+        <h2 >Welcome</h2>
+        <p>For a list of potential employers and links to their Home Page, About Page, Careers Page, LinkedIn and whether or not they are open to receiving a speculative CV, select <b>Organisations.</b></p>
+        <p>For a list of current vacancies from these employers, select <b>Job List.</b></p>
+        <p>Good luck on your search!</p>
     </div>
 );
 
